49db879843aefd2cb7d5cda1f0589f31bcbfa4b5
Paulo Matos <paulo.matos@itriad.org.br>
2017-02-15 15:23:29 -0400

    Player moving up and down

---
+++ Assets/Scripts/PlayerCrontroller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCrontroller : MonoBehaviour {
+
+	private Vector3 position;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y++;
+
+			if(position.y < -2.26f) {
+				this.transform.position = position;				
+			}
+
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y--;
+
+			if(position.y > -4.28f) {
+				this.transform.position = position;				
+			}
+		}
+	}
+
+}

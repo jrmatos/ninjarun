81fca0c1759c04552e59e12de3ac0df85cd99251
Paulo Matos <paulo.matos@itriad.org.br>
2017-02-15 15:04:20 -0400

    Ninja running on the street

---
+++ Assets/Scripts/MoveOffset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOffset : MonoBehaviour {
+	private Material currentMaterial;
+	public float speed;
+	private float offset;
+	// Use this for initialization
+	void Start () {
+		currentMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += 0.001f;
+		currentMaterial.SetTextureOffset ("_MainTex", new Vector2 (offset*speed, 0));
+	}
+}﻿

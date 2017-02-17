43d14bc11bdd7f9e9fedacf72208e52423f3a57c
Jeferson Barros <soldier.jhonson@gmail.com>
2017-02-17 01:00:26 -0400

    Create shuriken spawner and destroy outside layout

---
+++ Assets/Scripts/EnemyScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScene : MonoBehaviour {
+	
+	float speed;
+
+	void Start() {
+		speed = 4f;
+	}
+
+	void Update() {
+		//Get the enemy the current position
+		Vector2 position = transform.position;
+
+		//Compute the enemy new position
+		position = new Vector2 (position.x - speed * Time.deltaTime, position.y);
+
+		//Update the enemy position
+		transform.position = position;
+
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+
+		if (transform.position.x < min.x) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ShurikenSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShurikenSpawner : MonoBehaviour {
+
+	public GameObject EnemyPrefab;
+	float maxSpawnRateInSeconds = 5f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("SpawnEnemy", maxSpawnRateInSeconds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Function to spawn an enemy
+	void SpawnEnemy () {
+		//Instantiate an enemy
+		GameObject anEnemy = (GameObject)Instantiate (EnemyPrefab);
+		anEnemy.transform.position = new Vector2 (10,Random.Range (-4,-2));
+
+	}
+}

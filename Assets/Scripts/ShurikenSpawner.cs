e9de9f6965bcad2983b22b79c08f4d05d8fe3411
Jeferson Barros <soldier.jhonson@gmail.com>
2017-02-17 01:19:44 -0400

    Increase spawn rate every 30 seconds

--- Assets/Scripts/ShurikenSpawner.cs
+++ Assets/Scripts/ShurikenSpawner.cs
@@ -9,6 +9,9 @@ public class ShurikenSpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Invoke ("SpawnEnemy", maxSpawnRateInSeconds);
+
+		//Increase spawn rate every 30 seconds
+		InvokeRepeating("IncreaseSpawnRate",0f,30f);
 	}
 	
 	// Update is called once per frame
@@ -22,5 +25,29 @@ public class ShurikenSpawner : MonoBehaviour {
 		GameObject anEnemy = (GameObject)Instantiate (EnemyPrefab);
 		anEnemy.transform.position = new Vector2 (10,Random.Range (-4,-2));
 
+		//Schedule when to spawn next enemy
+		ScheduleNextEnemy();
+
+	}
+
+	void ScheduleNextEnemy() {
+		float spawnInNSeconds;
+
+		if (maxSpawnRateInSeconds > 1f) {
+			//pik a number between 1 and maxSpawnRateInSeconds
+			spawnInNSeconds = Random.Range (1f, maxSpawnRateInSeconds);
+		} else {
+			spawnInNSeconds = 1f;
+		}
+
+		Invoke ("SpawnEnemy", spawnInNSeconds);
+	}
+
+	//Function to increase the dificulty of the game
+	void IncreaseSpawnRate() {
+		if (maxSpawnRateInSeconds > 1f)
+			maxSpawnRateInSeconds--;
+		if (maxSpawnRateInSeconds == 1f)
+			CancelInvoke ("IncreaseSpawnRate");
 	}
 }
